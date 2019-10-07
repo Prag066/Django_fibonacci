cb08d3152d4d74b401c9319c18f82efe73f606fb
Prakhar <44711005+Prag066@users.noreply.github.com>
2019-10-07 13:56:09 +0530

    Update settings.py

--- fibsettings/fibsettings/settings.py
+++ fibsettings/fibsettings/settings.py
@@ -81,13 +81,25 @@ WSGI_APPLICATION = 'fibsettings.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'Prakhar',
+        'USER': 'Prakhar',
+        'PASSWORD': ' ',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
