dbdfe548b5beaa835d26604ad2deaef75f11db23
Prakhar <44711005+Prag066@users.noreply.github.com>
2019-10-07 13:31:46 +0530

    Update admin.py

--- fibsettings/fib/admin.py
+++ fibsettings/fib/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 
 from .models import Fib_data
 
-admin.site.register(Fib_data)
+class FibAdmin(admin.ModelAdmin):
+    list_display = ['number','duretion']
+admin.site.register(Fib_data,FibAdmin)
