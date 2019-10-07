38148f0765f94e258584f08a57b7c49709d52eaf
Prakhar <44711005+Prag066@users.noreply.github.com>
2019-10-07 14:18:32 +0530

    Update urls.py

--- fibsettings/fib/urls.py
+++ fibsettings/fib/urls.py
@@ -4,11 +4,12 @@ from rest_framework import routers
 from .views import FibViewset,UserViewSet
 
 router = routers.DefaultRouter()
-router.register(r'fib', FibViewset)
-router.register(r'users', UserViewSet)
+
 
 urlpatterns = [
     path('f1/',views.demo,name='demo'),
     path('Fibform/',views.Fibform,name='Fibform'),
     path('',views.main,name='f1'),
+    path('userlist-api/',UserViewSet.as_view({'get':'list'})),
+    path('fiblist-api/',FibViewset.as_view({'get':'list'})),
 ]
